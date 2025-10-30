aa96c5a7f0d02c591f110fefcfd5be5bd96d1efa
Aishraj30 <aishubamoriya@gmail.com>
2025-10-30 05:36:20 +0530

    Updated CORS settings for frontend access

--- server.js
+++ server.js
@@ -13,14 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: [  "http://localhost:5174",
-   https://face-frontend-five.vercel.app/,
-    ],
-
-    
-
-    credentials:true,
+  origin: [
+    "http://localhost:5174",
+    "https://face-frontend-five.vercel.app"
+  ],
+  credentials: true,
 }));
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); 
 
 
