0a03608b8db14ce9fe367a8455ea5968e08b6cd3
Aishraj30 <aishubamoriya@gmail.com>
2025-10-30 05:46:53 +0530

    Updated CORS settings for frontend access

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:5174",
-    "https://face-frontend-five.vercel.app"
+    "https://facerecogisation.vercel.app/"
   ],
   credentials: true,
 }));
